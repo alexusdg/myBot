0f3e22c8dae065c3ca784691c5d181aca8d9ec0f
alexusdg <adgore@email.sc.edu>
2021-07-17 13:42:41 -0400

    update

--- scheduledTwitterBot.py
+++ scheduledTwitterBot.py
@@ -4,5 +4,10 @@ from datetime import datetime, timedelta
 from threading import Timer
 import myTwitterBot
 
-schedule.every().day.at("10:00").do(myTwitterBot.tweetPin)
-schedule.every().day.at("18:00").do(myTwitterBot.tweetPin)
+schedule.every().day.at("12:00").do(myTwitterBot.tweetPin)
+schedule.every().day.at("17:00").do(myTwitterBot.tweetPin)
+
+while True:
+
+    schedule.run_pending()
+    time.sleep(2)
